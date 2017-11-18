3fea93cea9bcb9bf7ffa6d65a051e53483f60d35
IceBlade193 <32612145+IceBlade193@users.noreply.github.com>
2017-11-18 20:10:21 +0100

    Add files via upload

---
+++ Dom/Dom/House.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dom
+{
+    class House
+    {
+        public string Address;
+        public int Length;
+        public int WindowsAmount;
+        public int People;
+
+        public House(string address, int length, int windowsAmount, int people)
+        {
+            Address = address;
+            Length = length;
+            WindowsAmount = windowsAmount;
+            People = people;
+        }
+
+        public void Tax()
+        {
+            Console.WriteLine("Podatek za dom wynosi: " + Length*10);
+        }
+    }
+}
